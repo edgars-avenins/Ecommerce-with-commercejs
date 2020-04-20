4623369d64af85e92682c34efc1c072bfe17e013
Edgars <edgars.avenins@gmail.com>
2020-04-20 17:48:08 +1200

    display data over api from product list

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import Commerce from '@chec/commerce.js'
+import start from '../apis/api'
 
-const App = () => {
+let commerce = null
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      shopData: []
+    }
+  }
+
+  componentDidMount(){
+    start()
+      .then(() => {
+        commerce = new Commerce(process.env.API_KEY, true)
+        commerce.products.list()
+          .then(data => {
+            this.setState({
+              shopData: data.data
+            })
+          })
+      })
+  }
+
+
+render(){
   return (
-    <h1>React development has begun!</h1>
+    <>
+      {
+        this.state.shopData.map(item => {
+          return(
+            <>
+            <img src={item.media.source} alt="ulalala"/>
+            <h1>{item.name}</h1>
+            <p>{item.description}</p>
+            <p>{item.price.formatted_with_symbol}</p>
+            </>
+          )
+        })
+      }
+    </>
   )
+}
 }
 
 export default App
